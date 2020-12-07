3a2cf9f4de86828ce51ab587cd6bafe0927d70e4
Alexey Kutepov <alkutepov@gmail.com>
2020-12-07 19:59:01 +0300

    Lesson 3 part 1

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -1,7 +1,6 @@
 package ru.geekbrains.screen;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
@@ -10,37 +9,23 @@ import ru.geekbrains.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
-    private static final float V_LEN = 0.5f;
-
     private Texture img;
     private Vector2 pos;
-    private Vector2 touch;
-    private Vector2 v;
-    private Vector2 tmp;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
         pos = new Vector2();
-        touch = new Vector2();
-        v = new Vector2();
-        tmp = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        tmp.set(touch);
-        if (tmp.sub(pos).len() > v.len()) {
-            pos.add(v);
-        } else {
-            pos.set(touch);
-        }
         Gdx.gl.glClearColor(0.55f, 0.23f, 0.9f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, pos.x, pos.y);
+        batch.draw(img, pos.x - 0.5f, pos.y - 0.5f, 0.5f, 0.5f);
         batch.end();
     }
 
@@ -51,34 +36,7 @@ public class MenuScreen extends BaseScreen {
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v.set(touch.cpy().sub(pos)).setLength(V_LEN);
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        pos.set(screenX, Gdx.graphics.getHeight() - screenY);
-        return false;
-    }
-
-    @Override
-    public boolean keyDown(int keycode) {
-        switch (keycode) {
-            case Input.Keys.UP:
-                pos.y += 10;
-                break;
-            case Input.Keys.DOWN:
-                pos.y -= 10;
-                break;
-            case Input.Keys.RIGHT:
-                pos.x += 10;
-                break;
-            case Input.Keys.LEFT:
-                pos.x -= 10;
-                break;
-        }
-        return false;
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        return super.touchDown(touch, pointer, button);
     }
 }
